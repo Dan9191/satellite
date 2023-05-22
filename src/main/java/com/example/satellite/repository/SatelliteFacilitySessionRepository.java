ed1cf809e61416d0fee94efce0da7357376dd010
Julia Volkova <ulalien05@gmail.com>
2023-05-22 10:58:25 +0400

    add GreedyFacilityScheduleService to get a Queue of SatelliteFacilitySessions for this facility in chronological order without overlaps

--- src/main/java/com/example/satellite/repository/SatelliteFacilitySessionRepository.java
+++ src/main/java/com/example/satellite/repository/SatelliteFacilitySessionRepository.java
@@ -1,7 +1,6 @@
 package com.example.satellite.repository;
 
 import com.example.satellite.entity.Facility;
-import com.example.satellite.entity.Satellite;
 import com.example.satellite.entity.SatelliteFacilitySession;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
