47aaa17baed62a9dee9a4e960f012f7dc3ba55f8
Julia Volkova <ulalien05@gmail.com>
2023-05-23 15:21:44 +0400

    add facility schedule saving service to record the schedule into a file

---
+++ src/main/java/com/example/satellite/service/FacilityScheduleSavingService.java
@@ -0,0 +1,58 @@
+package com.example.satellite.service;
+
+import com.example.satellite.entity.SatelliteFacilitySession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class FacilityScheduleSavingService {
+
+    private final GreedyFacilityScheduleService scheduleService;
+    private long accessCounter = 0;
+
+    public void saveSchedule(String facility, File file){
+        List<SatelliteFacilitySession> schedule =
+                scheduleService.makeFacilitySchedule(facility);
+       try {
+           BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+           writer.write(getHeader(facility));
+           //needs data transferred per session information
+           schedule.stream()
+                   .filter(sfs -> sfs.getSatellite()!=null)
+                   .forEach(sfs -> {
+               try {
+                   writer.write(++accessCounter + getContent(sfs));
+                   writer.flush();
+               } catch (IOException e) {
+                   log.error("Возникла ошибка во время записи в файл.");
+               }
+           });
+           System.out.println("Запись завершена.");
+           writer.close();
+       } catch (IOException ex){
+           log.error("Возникла ошибка во время записи в файл.");
+       }
+
+    }
+
+    private String getHeader(String facility){
+        return facility + "\nAccess \tStartTime (UTCG) \t\t StopTime (UTCG) \t\t" +
+                "Duration (sec) \t\t Sat name \t\t Data (Mbytes) \n";
+    }
+
+    private String getContent(SatelliteFacilitySession session){
+        return "\t\t" + session.getStartSessionTime() + "\t" +
+                session.getEndSessionTime() + "\t" + session.getDuration() +
+                "\t\t" + session.getSatellite().getName() + "\n";
+    }
+
+}
