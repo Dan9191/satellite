1f686245f30cab0eec9cfe93731d23405c13f7df
Dan <example@email.com>
2023-05-28 03:57:09 +0400

    починио страничку

--- src/main/java/com/example/satellite/controller/MainController.java
+++ src/main/java/com/example/satellite/controller/MainController.java
@@ -5,7 +5,6 @@ import com.example.satellite.service.UploadFacilityFileService;
 import com.example.satellite.service.calculation.SchedulerCalculationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
